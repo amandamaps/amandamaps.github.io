08352f119ddb7bc35f607f4bbe365b6d59adf20c
amandamaps <amanda.carleski@icloud.com>
2025-09-25 06:39:01 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -28,3 +28,15 @@ $('.portfolio .carousel .carousel-item').each(function(){
         next.children(':first-child').clone().appendTo($(this));
       }
 });
+
+<script>
+document.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+</script>
+
